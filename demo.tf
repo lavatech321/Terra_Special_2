09c6be540559e4f0ffb6f9cb3276e127bdaf4e4b
Y Test <ytest@example.com>
2026-09-24 09:08:43 +0530

    new demo changes

--- demo.tf
+++ demo.tf
@@ -1,3 +1,15 @@
 resource "aws_security_group" "mysc" {
-
+	name = "mysc"
+	ingress {
+		to_port =
+		from_port =
+		protocol = 
+		cidr_block =
+	}
+	egress {
+	to_port =
+		from_port =
+		protocol = 
+		cidr_block =
+	}
 }
